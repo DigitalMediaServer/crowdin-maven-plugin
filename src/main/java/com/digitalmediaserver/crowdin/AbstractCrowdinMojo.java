UniversalMediaServer/ums-crowdin

Rename for Maven central

src/main/java/com/universalmediaserver/crowdin/AbstractCrowdinMojo.java
@@ -1,4 +1,4 @@
-package com.ums.crowdin.maven;
+package com.universalmediaserver.crowdin;
 
 import java.io.File;
 import java.io.InputStream;
@@ -30,7 +30,7 @@ import org.apache.maven.wagon.authentication.AuthenticationInfo;
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.input.SAXBuilder;
-import com.ums.crowdin.maven.tool.GitUtil;
+import com.universalmediaserver.crowdin.tool.GitUtil;
 
 public abstract class AbstractCrowdinMojo extends AbstractMojo {
 

src/main/java/com/universalmediaserver/crowdin/ApplyCrowdinMojo.java
@@ -1,4 +1,4 @@
-package com.ums.crowdin.maven;
+package com.universalmediaserver.crowdin;
 
 import java.io.File;
 import java.io.FileFilter;

src/main/java/com/universalmediaserver/crowdin/FetchCrowdinMojo.java
@@ -1,4 +1,4 @@
-package com.ums.crowdin.maven;
+package com.universalmediaserver.crowdin;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -32,10 +32,10 @@ import org.apache.maven.shared.dependency.tree.DependencyTreeBuilderException;
 import org.apache.maven.shared.dependency.tree.traversal.CollectingDependencyNodeVisitor;
 import org.jdom.Document;
 import org.jdom.Element;
-import com.ums.crowdin.maven.tool.CodeConversion;
-import com.ums.crowdin.maven.tool.SortedProperties;
-import com.ums.crowdin.maven.tool.SpecialArtifact;
-import com.ums.crowdin.maven.tool.TranslationFile;
+import com.universalmediaserver.crowdin.tool.CodeConversion;
+import com.universalmediaserver.crowdin.tool.SortedProperties;
+import com.universalmediaserver.crowdin.tool.SpecialArtifact;
+import com.universalmediaserver.crowdin.tool.TranslationFile;
 
 /**
  *Fetch crowdin translations in this project, looking dependencies

src/main/java/com/universalmediaserver/crowdin/PushCrowdinMojo.java
@@ -1,4 +1,4 @@
-package com.ums.crowdin.maven;
+package com.universalmediaserver.crowdin;
 
 import java.io.File;
 import java.util.HashMap;

src/main/java/com/universalmediaserver/crowdin/tool/CodeConversion.java
@@ -1,4 +1,4 @@
-package com.ums.crowdin.maven.tool;
+package com.universalmediaserver.crowdin.tool;
 
 import java.util.Locale;
 

src/main/java/com/universalmediaserver/crowdin/tool/GitUtil.java
@@ -1,4 +1,4 @@
-package com.ums.crowdin.maven.tool;
+package com.universalmediaserver.crowdin.tool;
 
 import java.io.File;
 import java.io.IOException;

src/main/java/com/universalmediaserver/crowdin/tool/SortedProperties.java
@@ -1,4 +1,4 @@
-package com.ums.crowdin.maven.tool;
+package com.universalmediaserver.crowdin.tool;
 
 import java.io.BufferedWriter;
 import java.io.IOException;

src/main/java/com/universalmediaserver/crowdin/tool/SpecialArtifact.java
@@ -1,4 +1,4 @@
-package com.ums.crowdin.maven.tool;
+package com.universalmediaserver.crowdin.tool;
 
 import java.io.File;
 import java.util.Collection;

DigitalMediaServer/crowdin-maven-plugin

src/main/java/com/digitalmediaserver/crowdin/AbstractCrowdinMojo.java
@@ -1,4 +1,4 @@
-package com.ums.crowdin.maven;
+package com.digitalmediaserver.crowdin;
 
 import java.io.File;
 import java.io.InputStream;
@@ -30,7 +30,7 @@ import org.apache.maven.wagon.authentication.AuthenticationInfo;
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.input.SAXBuilder;
-import com.ums.crowdin.maven.tool.GitUtil;
+import com.digitalmediaserver.crowdin.tool.GitUtil;
 
 public abstract class AbstractCrowdinMojo extends AbstractMojo {
 

src/main/java/com/digitalmediaserver/crowdin/ApplyCrowdinMojo.java
@@ -1,4 +1,4 @@
-package com.ums.crowdin.maven;
+package com.digitalmediaserver.crowdin;
 
 import java.io.File;
 import java.io.FileFilter;

src/main/java/com/digitalmediaserver/crowdin/FetchCrowdinMojo.java
@@ -1,4 +1,4 @@
-package com.ums.crowdin.maven;
+package com.digitalmediaserver.crowdin;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -32,10 +32,10 @@ import org.apache.maven.shared.dependency.tree.DependencyTreeBuilderException;
 import org.apache.maven.shared.dependency.tree.traversal.CollectingDependencyNodeVisitor;
 import org.jdom.Document;
 import org.jdom.Element;
-import com.ums.crowdin.maven.tool.CodeConversion;
-import com.ums.crowdin.maven.tool.SortedProperties;
-import com.ums.crowdin.maven.tool.SpecialArtifact;
-import com.ums.crowdin.maven.tool.TranslationFile;
+import com.digitalmediaserver.crowdin.tool.CodeConversion;
+import com.digitalmediaserver.crowdin.tool.SortedProperties;
+import com.digitalmediaserver.crowdin.tool.SpecialArtifact;
+import com.digitalmediaserver.crowdin.tool.TranslationFile;
 
 /**
  *Fetch crowdin translations in this project, looking dependencies

src/main/java/com/digitalmediaserver/crowdin/PushCrowdinMojo.java
@@ -1,4 +1,4 @@
-package com.ums.crowdin.maven;
+package com.digitalmediaserver.crowdin;
 
 import java.io.File;
 import java.util.HashMap;

src/main/java/com/digitalmediaserver/crowdin/tool/CodeConversion.java
@@ -1,4 +1,4 @@
-package com.ums.crowdin.maven.tool;
+package com.digitalmediaserver.crowdin.tool;
 
 import java.util.Locale;
 

src/main/java/com/digitalmediaserver/crowdin/tool/GitUtil.java
@@ -1,4 +1,4 @@
-package com.ums.crowdin.maven.tool;
+package com.digitalmediaserver.crowdin.tool;
 
 import java.io.File;
 import java.io.IOException;

src/main/java/com/digitalmediaserver/crowdin/tool/SortedProperties.java
@@ -1,4 +1,4 @@
-package com.ums.crowdin.maven.tool;
+package com.digitalmediaserver.crowdin.tool;
 
 import java.io.BufferedWriter;
 import java.io.IOException;

src/main/java/com/digitalmediaserver/crowdin/tool/SpecialArtifact.java
@@ -1,4 +1,4 @@
-package com.ums.crowdin.maven.tool;
+package com.digitalmediaserver.crowdin.tool;
 
 import java.io.File;
 import java.util.Collection;